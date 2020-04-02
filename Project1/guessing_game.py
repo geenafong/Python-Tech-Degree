430aaee73febf34a59b8e21911330df25234b4bc
geenafong <geenafong@gmail.com>
2020-04-02 16:36:36 -0700

    Trying for exceeds

--- Project1/guessing_game.py
+++ Project1/guessing_game.py
@@ -11,6 +11,11 @@ NOTE: If you strongly prefer to work locally on your own computer, you can total
 
 import random
 
+high_score_list = []
+
+def get_random_number():
+    random_number = random.randint(1,10)
+    return random_number
 
 def start_game():
     """Psuedo-code Hints
@@ -31,42 +36,39 @@ def start_game():
     """
     # write your code inside this function.
     print("Welcome to the number guessing game! Let's begin...")
+    if len(high_score_list) == 0:
+        print("No current high score")
+    else:
+        print("Current highscore is: ", str(min(high_score_list)))
     guess_number = ""
-    random_number = random.randint(1,10)
-    game = 0
+    random_number = get_random_number()
     count = 1
-    score_list = []
-    while guess_number == "":
+
+    while guess_number != random_number:
         try:
             guess_number = int(input("Please guess a number from 1 to 10: "))
         except ValueError:
             print("Oops!  That was not a valid number.  Try again...")
-
-    while guess_number != random_number:
-        count+=1
-        try:
+        else:
             if guess_number > 10 or guess_number < 1:
                 print("Your number was not between 1 to 10.")
             elif guess_number > random_number:
+                count+=1
                 print("It is lower.")
             elif guess_number < random_number:
+                count+=1
                 print("It is higher.")
-            guess_number = int(input("Try again: "))
-
-        except ValueError:
-            print("Oops!  That was not a valid number.  Try again...")
-    print(f'Congratulations! You got it. It only took you {count} try(ies). The game is now over.')
-    score_list.append(count)
-    print("Your high score is", score_list[0], ". Number in list: ", len(score_list))
-    
-    play_again = str(input("See if you can beat your score. Play again? yes or no: ").lower())
-    
-    if play_again == "yes":
-        start_game()
-        
-    else:
-        print("Thanks for playing!")
-
+            else:
+                print(f'Congratulations! You got it. It only took you {count} try(ies). The game is now over.')
+                current_score = count
+                high_score_list.append(current_score)
+                print("Current highscore is: ", str(min(high_score_list)))
+                play_again = str(input("See if you can beat your score. Play again? yes or no: ").lower())
+                if play_again == "yes":
+                        start_game()
+                else:
+                    print("Thanks for playing")
+                    break
 
 if __name__ == '__main__':
     # Kick off the program by calling the start_game function.
